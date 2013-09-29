131e626fe56db6b203f52bd0427e3e0d38c0c6cf
James Mardell <james.mardell@imperial.ac.uk>
2013-09-29 23:24:14 +0100

    All three noise algorithms working correctly.

--- profiling/ECGLowSNR.m
+++ profiling/ECGLowSNR.m
@@ -1,33 +1,34 @@
-function [output] = ECGLowSaturation(input)
-%ECGLOWSATURATION Summary of this function goes here
+function [output] = ECGLowSNR(input)
+%ECGLOWSNR Summary of this function goes here
 %   Detailed explanation goes here
     % Parameters
     ecg_high_saturation = 949;
     ecg_low_saturation = 74;
     ecg_packet_length = 63;
+    ecg_quantisation_noise = 5;
+    
+    previous_packet_range = 0;
     
     % Create a map with an integer key (Bug: 1-71KD5H)
     output = containers.Map(uint32(1) , uint32(1) );
     remove(output, 1);
     bad_packets = [];
-
-    % Find saturated individual samples that lie on the maximum or minimum
-    saturated_samples = zeros(size(input) ); % default is false
-    saturated_samples(find(input > (ecg_high_saturation - 1) | input < (ecg_low_saturation + 1 ) ) ) = 1;
     
     % Indicate which packets are bad
     for p = 1 : floor(length(input) / ecg_packet_length)
         start_index = 1 + ((p - 1) * ecg_packet_length);
         end_index = (p * ecg_packet_length);
         
-        packet = saturated_samples(start_index : end_index);
-        packet_is_saturated = 1 - isempty(find(packet == 1, 1) );
+        packet = input(start_index : end_index);
+        packet_range = max(input) - min(input);
         
         % Add the packet to the output
-        if (packet_is_saturated == 1)
+        if (max([previous_packet_range packet_range] <= ecg_quantisation_noise) )
             bad_packets = [ bad_packets (start_index - 1) ];
         end
     end % foreach packet
+    
+    previous_packet_range = packet_range;
     
     % Convert list of bad packets into indicies with durations
     if (numel(bad_packets) ~= 0)
